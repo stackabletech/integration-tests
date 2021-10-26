acc727aece49a87ed08a210de28d5c2a7dbc8809
Nikolaus Winter <mail@nikolaus-winter.de>
2021-10-26 16:53:20 +0200

    operator integration tests: adjusted script to run in  Jenkins CI from new repo (#7)

--- kafka-operator/.ci/integration-tests/aws-centos-8/test.sh
+++ kafka-operator/.ci/integration-tests/aws-centos-8/test.sh
@@ -2,8 +2,8 @@
 /stackable.sh testdriver-1 -i /.cluster/key 'curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y'
 /stackable.sh testdriver-1 -i /.cluster/key 'cargo --version'
 /stackable.sh testdriver-1 -i /.cluster/key 'sudo yum install vim procps curl gcc make pkgconfig openssl-devel systemd-devel python3-pip container-selinux selinux-policy-base git -y'
-/stackable.sh testdriver-1 -i /.cluster/key "git clone -b $GIT_BRANCH https://github.com/stackabletech/kafka-operator-integration-tests.git"
-/stackable.sh testdriver-1 -i /.cluster/key 'cd kafka-operator-integration-tests/ && cargo test -- --test-threads=1'
+/stackable.sh testdriver-1 -i /.cluster/key "git clone -b $GIT_BRANCH https://github.com/stackabletech/integration-tests.git"
+/stackable.sh testdriver-1 -i /.cluster/key 'cd integration-tests/ && cargo test --package kafka-operator-integration-tests -- --test-threads=1'
 exit_code=$?
 /stackable.sh main-1 -i /.cluster/key 'journalctl -u stackable-agent' > /target/main-1-stackable-agent.log
 /stackable.sh main-2 -i /.cluster/key 'journalctl -u stackable-agent' > /target/main-2-stackable-agent.log
