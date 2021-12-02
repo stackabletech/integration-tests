5b13d61c30206128e729daaa8a638135b3a7c9f0
Nikolaus Winter <mail@nikolaus-winter.de>
2021-12-02 20:49:02 +0100

    CI: OPA/Spark no longer multi-threaded

--- opa-operator/.ci/integration-tests/hcloud-centos-8/test.sh
+++ opa-operator/.ci/integration-tests/hcloud-centos-8/test.sh
@@ -2,7 +2,7 @@ ssh testdriver-1 'curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh
 ssh testdriver-1 'cargo --version'
 ssh testdriver-1 'sudo yum install vim procps curl gcc make pkgconfig openssl-devel systemd-devel python3-pip container-selinux selinux-policy-base git -y'
 ssh testdriver-1 "git clone -b $GIT_BRANCH https://github.com/stackabletech/integration-tests.git"
-ssh testdriver-1 'cd integration-tests/opa-operator && cargo test -- --nocapture'
+ssh testdriver-1 'cd integration-tests/opa-operator && cargo test -- --nocapture --test-threads=1'
 exit_code=$?
 ./operator-logs.sh opa > /target/opa-operator.log
 exit $exit_code
