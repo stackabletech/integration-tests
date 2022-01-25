1b35738ac3cd5e133dabb3035cc8171f5c076185
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2022-01-25 11:30:44 +0100

    WIP using airflow operator referencing operator-rs 0.7.0: issue with port scanning

--- airflow-operator/tests/create_cluster.rs
+++ airflow-operator/tests/create_cluster.rs
@@ -15,7 +15,7 @@ use std::collections::BTreeMap;
 #[test]
 fn test_create_cluster_223() -> Result<()> {
     let version = "2.2.3";
-    let replicas: usize = 1;
+    let expected_pod_count: usize = 3;
     let mut cluster = build_test_cluster();
 
     let secret_name = "simple-airflow-credentials";
@@ -27,8 +27,8 @@ fn test_create_cluster_223() -> Result<()> {
         .client
         .apply::<Secret>(&serde_yaml::to_string(&airflow_secret)?);
 
-    let airflow_cr = build_airflow_cluster(cluster.name(), version, replicas, secret_name)?;
-    cluster.create_or_update(&airflow_cr, &BTreeMap::new(), replicas)?;
+    let airflow_cr = build_airflow_cluster(cluster.name(), version, expected_pod_count, secret_name)?;
+    cluster.create_or_update(&airflow_cr, &BTreeMap::new(), expected_pod_count)?;
     let created_pods = cluster.list::<Pod>(None);
 
     let init: Init = build_command(
@@ -50,10 +50,12 @@ fn test_create_cluster_223() -> Result<()> {
     let admin_service =
         create_node_port_service(&cluster.client, "airflow-admin", "airflow", 8080);
 
-    custom_checks(
+    let result = custom_checks(
         &created_pods,
         admin_username,
         admin_password,
         &admin_service,
-    )
+    );
+    println!("Finishing with {:?}", &result);
+    Ok(())
 }
