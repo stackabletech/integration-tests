9b01afc84d8738dc3e67024f964366f04f9d0360
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2022-02-01 15:39:08 +0100

    Airflow integration tests (#96)
    * WIP initial code, not yet compiling due to missing trait implementation
    
    * WIP using airflow operator referencing operator-rs 0.7.0: issue with port scanning
    
    * WIP filter on service pods
    
    * working tests
    
    * removed ci files
    
    * WIP initial code, not yet compiling due to missing trait implementation
    
    * WIP using airflow operator referencing operator-rs 0.7.0: issue with port scanning
    
    * WIP filter on service pods
    
    * working tests
    
    * removed ci files
    
    * minor changes made, python script extended for airflow
    
    * updated to airflow-operator main branch
    
    * added comments, sorted and pruned .toml entries
    
    * removed unused deps
    
    * added airflow tests to deny.toml exceptions
    
    * added crd to deny file
    
    * fixes: list ordering, owner-ref for init command, named arguments
    
    * clippy fix
    
    * WIP initial code, not yet compiling due to missing trait implementation
    
    * WIP using airflow operator referencing operator-rs 0.7.0: issue with port scanning
    
    * WIP filter on service pods
    
    * working tests
    
    * removed ci files
    
    * WIP initial code, not yet compiling due to missing trait implementation
    
    * WIP using airflow operator referencing operator-rs 0.7.0: issue with port scanning
    
    * WIP filter on service pods
    
    * working tests
    
    * removed ci files
    
    * minor changes made, python script extended for airflow
    
    * updated to airflow-operator main branch
    
    * added comments, sorted and pruned .toml entries
    
    * removed unused deps
    
    * added airflow tests to deny.toml exceptions
    
    * added crd to deny file
    
    * fixes: list ordering, owner-ref for init command, named arguments
    
    * clippy fix
    
    * airflow: commented out superset from top-level toml

---
+++ airflow-operator/tests/common/checks.rs
@@ -0,0 +1,37 @@
+use anyhow::{anyhow, Result};
+use integration_test_commons::operator::checks;
+use integration_test_commons::operator::service::TemporaryService;
+use integration_test_commons::stackable_operator::kube::ResourceExt;
+use integration_test_commons::test::prelude::Pod;
+use reqwest::blocking::Client;
+
+/// Execute and consolidate check results to be performed on the airflow node services.
+pub fn custom_checks(service_pods: &[Pod], service: &TemporaryService) -> Result<()> {
+    for service_pod in service_pods {
+        checks::scan_port(&service.address(service_pod))?;
+        health_check(service, service_pod)?;
+    }
+    Ok(())
+}
+
+/// Invoke the webserver health endpoint.
+pub fn health_check(service: &TemporaryService, pod: &Pod) -> Result<()> {
+    let client = Client::new();
+
+    let address = service.address(pod);
+
+    let response = client
+        .get(format!("http://{}/api/v1/health", address))
+        .send()?;
+    println!("Returned {}, {:?}", &response.status(), &response);
+
+    if response.status().is_success() {
+        Ok(())
+    } else {
+        Err(anyhow!(
+            "Health-check on pod [{}] failed. {:?}",
+            pod.name(),
+            response
+        ))
+    }
+}
