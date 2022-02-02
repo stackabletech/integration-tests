4ff385214337edbf2260e150f0c287e391d5bd4b
Malte Sander <malte.sander.it@gmail.com>
2022-02-02 17:35:27 +0100

    Added regorule operator to trino dependencies.

--- create_test_cluster.py
+++ create_test_cluster.py
@@ -270,6 +270,7 @@ def install_dependencies_airflow():
 def install_dependencies_trino():
   install_stackable_operator("hive")
   install_stackable_operator("opa")
+  install_stackable_operator("regorule")
 
   helper_add_helm_repo("minio", "https://operator.min.io")
   release = helper_find_helm_release("minio-operator", "minio-operator")
