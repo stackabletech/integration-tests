2a1cc37c4e246e3a455b9f25be9c27e74bcf5654
Nikolaus Winter <mail@nikolaus-winter.de>
2022-02-03 09:32:36 +0100

    CI HDFS w/ Kuttl: fixed log retrieval

--- hdfs-operator/.ci/integration-tests/hcloud-centos-8/test.sh
+++ hdfs-operator/.ci/integration-tests/hcloud-centos-8/test.sh
@@ -1,6 +1,5 @@
 git clone -b $GIT_BRANCH https://github.com/stackabletech/integration-tests.git
-cd integration-tests/hdfs-operator
-kubectl kuttl test
+(cd integration-tests/hdfs-operator && kubectl kuttl test)
 exit_code=$?
 ./operator-logs.sh hdfs > /target/hdfs-operator.log
 exit $exit_code
