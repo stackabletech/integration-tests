82e3a81eeb3ef736166d6a852d8874f0651d16d2
Nikolaus Winter <mail@nikolaus-winter.de>
2022-02-07 15:39:40 +0100

    CI/HDFS: added Azure AKS based test

--- zookeeper-operator/.ci/integration-tests/azure-aks/test.sh
+++ zookeeper-operator/.ci/integration-tests/azure-aks/test.sh
@@ -1,4 +1,3 @@
-kubectl get nodes > /target/k8s_nodes.txt
 git clone -b $GIT_BRANCH https://github.com/stackabletech/integration-tests.git
 (cd integration-tests/zookeeper-operator && kubectl kuttl test)
 exit_code=$?
