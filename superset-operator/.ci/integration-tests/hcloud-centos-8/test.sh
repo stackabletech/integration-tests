71fe080fda4a9a5e17d542ffa8fa7c0ce81309bc
Nikolaus Winter <mail@nikolaus-winter.de>
2022-02-07 14:16:43 +0100

    CI/Superset: adjusted test definition to kuttl

---
+++ superset-operator/.ci/integration-tests/hcloud-centos-8/test.sh
@@ -0,0 +1,28 @@
+# Bitnami Helm repo to install collaborators
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update bitnami
+
+# Install Postgres
+helm install superset-postgresql bitnami/postgresql \
+    --version 11.0.0 \
+    --set auth.username=superset \
+    --set auth.password=superset \
+    --set auth.database=superset
+
+# Wait for Postgres to be up and running
+echo Starting Postgresql database ...
+while [ "$(kubectl get pod superset-postgresql-0 --output=jsonpath='{.status.containerStatuses[0].ready}')" != "true" ]; do
+	sleep 2
+done
+echo Postgresql database started.
+echo
+sleep 30
+
+# Execute tests
+git clone -b $GIT_BRANCH https://github.com/stackabletech/integration-tests.git
+(cd integration-tests/superset-operator && kubectl kuttl test)
+exit_code=$?
+
+# save logfiles and exit
+./operator-logs.sh superset > /target/superset-operator.log
+exit $exit_code
