b0b7a7601a933dc70d0cc205110829983cae979f
Nikolaus Winter <mail@nikolaus-winter.de>
2022-02-08 08:16:54 +0100

    CI: cleanup after migration to kuttl

--- spark-operator/.ci/integration-tests/azure-docker/test.sh
+++
@@ -1,6 +0,0 @@
-docker run --rm --volume $KUBECONFIG_FOLDER_VOLUME:/root/.kube \
-    docker.stackable.tech/integration-tests:$TEST_IMAGE_VERSION \
-    spark-operator-integration-tests -- --nocapture --test-threads=1
-exit_code=$?
-./operator-logs.sh spark > /target/spark-operator.log
-exit $exit_code
