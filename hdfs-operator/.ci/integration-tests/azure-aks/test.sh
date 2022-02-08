9b99ef558ea8c81ba0ab0cf5abb172612b8cf7ae
Nikolaus Winter <mail@nikolaus-winter.de>
2022-02-08 09:11:52 +0100

    CI: added zookeeper logs to HDFS managed k8s tests

--- hdfs-operator/.ci/integration-tests/azure-aks/test.sh
+++ hdfs-operator/.ci/integration-tests/azure-aks/test.sh
@@ -2,4 +2,5 @@ git clone -b $GIT_BRANCH https://github.com/stackabletech/integration-tests.git
 (cd integration-tests/hdfs-operator && kubectl kuttl test)
 exit_code=$?
 ./operator-logs.sh hdfs > /target/hdfs-operator.log
+./operator-logs.sh zookeeper > /target/zookeeper-operator.log
 exit $exit_code
