641ad6e33ea4638445367e600e243a5dd77d94e8
Nikolaus Winter <mail@nikolaus-winter.de>
2022-02-10 22:40:11 +0100

    added dummy-operator and changed zookeeper to test stuff on Argo Workflows

--- zookeeper-operator/.ci/integration-tests/hcloud-centos-8/test.sh
+++ zookeeper-operator/.ci/integration-tests/hcloud-centos-8/test.sh
@@ -1,5 +1,4 @@
-git clone -b $GIT_BRANCH https://github.com/stackabletech/integration-tests.git
-(cd integration-tests/zookeeper-operator && kubectl kuttl test)
+(cd /test-suite && kubectl kuttl test)
 exit_code=$?
 ./operator-logs.sh zookeeper > /target/zookeeper-operator.log
 exit $exit_code
