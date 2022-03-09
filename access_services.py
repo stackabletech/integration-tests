6826f9a1732798eb5f90071031da6d13a7fe51b7
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2022-03-09 15:19:20 +0100

    Fix linter warnings (beside Line too long)

--- access_services.py
+++ access_services.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-
-# To install the requirements run "pip install -r forward_services_locally_requirements.txt"
+"""
+Enables access to services deployed on the Stackable platform
+To install the requirements run "pip install -r forward_services_locally_requirements.txt"
+"""
 
 import argparse
 import os
@@ -43,38 +45,42 @@ EXTRA_INFO = {
     r".*superset.*-external$": 'kubectl get secret $(kubectl get supersetclusters.superset.stackable.tech $(echo "$SERVICE_NAME" | sed "s/-external$//") --template=\'{{.spec.credentialsSecret}}\') --template=\'user: {{index .data "adminUser.username" | base64decode}}, password: {{index .data "adminUser.password" | base64decode}}\'',
 }
 
-k8s = None
-k8s_node_ips = {}
+K8S = None
+K8S_NODE_IPS = {}
 
-processes = []
-forwarded_services = []
+PROCESSES = []
+SERVICES = []
+
 
 def check_args() -> Namespace:
-  parser = argparse.ArgumentParser(
-    description="This tool can be used to forward services of Stackable products to the local machine."
-  )
-  parser.add_argument('--namespace', '-n', required=False,
-                      help='The namespace of the services to forward. As a default the current kubectl context will be used')
-  parser.add_argument('--all-namespaces', '-a', action='store_true',
-                      help='Forward services from all namespaces')
-  parser.add_argument('--all-services', action='store_true',
-                      help='Forward all services regardles of the name or the exposed ports')
-  parser.add_argument('--dont-use-port-forward', '-d', action='store_true',
-                      help='Dont use "kubectl port-forward", instead return the NodeIP and NodePort. This may cause problems as many services dont have the nodePorts attribute or services having multiple endpoints')
-  parser.add_argument('--verbose', '-v', action='store_true',
-                      help='In case of using port-forward show stdout output of "kubectl port-forward" command')
-  return parser.parse_args()
+    """Parse the given CLI arguments"""
+    parser = argparse.ArgumentParser(
+        description="This tool can be used to forward services of Stackable products to the local machine."
+    )
+    parser.add_argument('--namespace', '-n', required=False,
+                        help='The namespace of the services to forward. As a default the current kubectl context will be used')
+    parser.add_argument('--all-namespaces', '-a', action='store_true',
+                        help='Forward services from all namespaces')
+    parser.add_argument('--all-services', action='store_true',
+                        help='Forward all services regardles of the name or the exposed ports')
+    parser.add_argument('--dont-use-port-forward', '-d', action='store_true',
+                        help='Dont use "kubectl port-forward", instead return the NodeIP and NodePort. This may cause problems as many services dont have the nodePorts attribute or services having multiple endpoints')
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help='In case of using port-forward show stdout output of "kubectl port-forward" command')
+    return parser.parse_args()
+
 
 def main():
-    global k8s, k8s_nodes
+    """Main function executed. Blocking (waits for all processes to finish)."""
+    global K8S, K8S_NODE_IPS
 
     args = check_args()
 
     config.load_kube_config()
     namespace = args.namespace or config.list_kube_config_contexts()[1]['context'].get("namespace", "default")
-    k8s = client.CoreV1Api()
+    K8S = client.CoreV1Api()
 
-    for node in k8s.list_node().items:
+    for node in K8S.list_node().items:
         node_name = node.metadata.name
         node_ip = None
         for address in node.status.addresses:
@@ -84,12 +90,12 @@ def main():
         if node_ip is None:
             raise Exception(f"Could not find a valid InternalIP or ExternalIP for node {node_name}")
 
-        k8s_node_ips[node_name] = node_ip
+        K8S_NODE_IPS[node_name] = node_ip
 
     if args.all_namespaces:
-        services = k8s.list_service_for_all_namespaces()
+        services = K8S.list_service_for_all_namespaces()
     else:
-        services = k8s.list_namespaced_service(namespace=namespace)
+        services = K8S.list_namespaced_service(namespace=namespace)
 
     for service_spec in services.items:
         service_namespace = service_spec.metadata.namespace
@@ -101,37 +107,46 @@ def main():
             service_node_port = port_spec.node_port or '<no nodePort attribute on service>'
             if shall_expose_service(service_name, service_port_name) or args.all_services:
                 if args.dont_use_port_forward:
-                    calculate_node_address(service_namespace, service_name, service_port, service_port_name, service_node_port)
+                    calculate_node_address(service_namespace, service_name, service_port, service_port_name,
+                                           service_node_port)
                 else:
                     forward_port(service_namespace, service_name, service_port, service_port_name, args.verbose)
 
-    print(tabulate(forwarded_services, headers=['Namespace', 'Service', 'Port', 'Name', 'URL', 'Extra info'], tablefmt='psql'))
+    print(tabulate(SERVICES, headers=['Namespace', 'Service', 'Port', 'Name', 'URL', 'Extra info'],
+                   tablefmt='psql'))
     print()
 
-    for process in processes:
+    for process in PROCESSES:
         process.wait()
 
+
 def shall_expose_service(service_name, service_port_name) -> bool:
+    """Should the particular port of the services be exposed?"""
     for regex, port_names_to_expose in SERVICES_TO_EXPOSE.items():
         if re.match(regex, service_name) \
-            and (service_port_name in port_names_to_expose or len(port_names_to_expose) == 0):
+                and (service_port_name in port_names_to_expose or len(port_names_to_expose) == 0):
             return True
     return False
 
+
 def find_free_port() -> int:
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-        s.bind(('localhost', 0))
-        return s.getsockname()[1]
+    """Find an unused ports that can use to bind port-forwarding to"""
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        sock.bind(('localhost', 0))
+        return sock.getsockname()[1]
+
 
 def forward_port(service_namespace, service_name, service_port, service_port_name, verbose):
+    """Execute a "kubectl port-forward" command to forward to specified service. Non-clocking (spawns process in background). """
     local_port = find_free_port()
-    command = ["kubectl", "-n", service_namespace, "port-forward", f"service/{service_name}", f"{local_port}:{service_port}"]
+    command = ["kubectl", "-n", service_namespace, "port-forward", f"service/{service_name}",
+               f"{local_port}:{service_port}"]
     if verbose:
         process = subprocess.Popen(command)
     else:
         process = subprocess.Popen(command, stdout=open(os.devnull, 'wb'))
-    processes.append(process)
-    forwarded_services.append([
+    PROCESSES.append(process)
+    SERVICES.append([
         service_namespace,
         service_name,
         service_port,
@@ -140,12 +155,14 @@ def forward_port(service_namespace, service_name, service_port, service_port_nam
         get_extra_info(service_name),
     ])
 
-def calculate_node_address(service_namespace, service_name, service_port, service_port_name, service_node_port):
-    endpoint = k8s.read_namespaced_endpoints(namespace=service_namespace, name=service_name)
-    node_name = endpoint.subsets[0].addresses[0].node_name
-    node_ip = k8s_node_ips[node_name]
 
-    forwarded_services.append([
+def calculate_node_address(service_namespace, service_name, service_port, service_port_name, service_node_port):
+    """Calculate the NodeIP and NodePort combination so that the service can be accessed directly without a port-forward"""
+    endpoint = K8S.read_namespaced_endpoints(namespace=service_namespace, name=service_name)
+    node_name = endpoint.subsets[0].addresses[0].node_name
+    node_ip = K8S_NODE_IPS[node_name]
+
+    SERVICES.append([
         service_namespace,
         service_name,
         service_port,
@@ -154,23 +171,27 @@ def calculate_node_address(service_namespace, service_name, service_port, servic
         get_extra_info(service_name),
     ])
 
-def get_extra_info(service_name):
+
+def get_extra_info(service_name) -> str:
+    """Run the configured shell command to get some addition infos (like credentials) that we can show the user"""
     command = None
     for regex, command_from_loop in EXTRA_INFO.items():
         if re.match(regex, service_name):
             command = command_from_loop
     if command is None:
         return ""
-    else:
-        complete_command = f"SERVICE_NAME={service_name} && {command}"
-        return subprocess.run(complete_command, shell=True, capture_output=True).stdout.decode('utf-8')
+    complete_command = f"SERVICE_NAME={service_name} && {command}"
+    return subprocess.run(complete_command, shell=True, capture_output=True, check=False).stdout.decode('utf-8')
+
 
 def cleanup():
-    if len(processes) > 0:
-        print(f"Stopping {len(processes)} port-forwards")
-        for process in processes:
+    """Stops the running background processes"""
+    if len(PROCESSES) > 0:
+        print(f"Stopping {len(PROCESSES)} port-forwards")
+        for process in PROCESSES:
             process.kill()
-        processes.clear()
+        PROCESSES.clear()
+
 
 if __name__ == '__main__':
     try:
