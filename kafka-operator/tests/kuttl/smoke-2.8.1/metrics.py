8e561fd55c4dcb228928226112f95f199179d9fc
Teo Klestrup Röijezon <teo@nullable.se>
2022-03-17 10:02:55 +0100

    Bring back Kafka 2.8.1 tests using copy/paste (#178)
    * Bring back Kafka 2.8.1 tests using copy/paste
    
    * Added missing --- to make the linter happy.
    
    * more linter warnings
    
    * missing newline at end of python files
    
    Co-authored-by: Sönke Liebau <soenke.liebau@stackable.de>

--- kafka-operator/tests/kuttl/smoke-2.8.1/metrics.py
+++ kafka-operator/tests/kuttl/smoke-2.8.1/metrics.py
@@ -5,7 +5,7 @@ import logging
 if __name__ == "__main__":
     result = 0
 
-    log_level = 'DEBUG' ### if args.debug else 'INFO'
+    log_level = 'DEBUG'  # if args.debug else 'INFO'
     logging.basicConfig(level=log_level, format='%(asctime)s %(levelname)s: %(message)s', stream=sys.stdout)
 
     http_code = requests.get("http://simple-kafka-broker-default:9606").status_code
