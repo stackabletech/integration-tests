ede2846cdb621786ae299a6b05ffe83f1812b525
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2022-03-21 16:36:13 +0100

    Fix openldap version

--- superset-operator/tests/kuttl/ldap-1.4.1/create_ldap_user.sh
+++ superset-operator/tests/kuttl/ldap-1.4.1/create_ldap_user.sh
@@ -4,7 +4,7 @@
 # To check the new user
 # ldapsearch -H ldap://localhost:1389 -D cn=integrationtest,ou=users,dc=example,dc=org -w integrationtest -b ou=users,dc=example,dc=org
 
-cat << 'EOF' | ldapadd  -H ldap://localhost:1389 -D cn=admin,dc=example,dc=org -w admin
+cat << 'EOF' | ldapadd -H ldap://localhost:1389 -D cn=admin,dc=example,dc=org -w admin
 dn: cn=integrationtest,ou=users,dc=example,dc=org
 objectClass: inetOrgPerson
 objectClass: posixAccount
