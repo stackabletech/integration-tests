7c7fe0a90ea843a9655b52043f85d669b3f2b055
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2022-05-11 11:09:43 +0200

    Update to latest implementation

--- superset-operator/tests/kuttl/ldap-1.4.1/login.py
+++ superset-operator/tests/kuttl/ldap-1.4.1/login.py
@@ -8,15 +8,14 @@ if __name__ == "__main__":
     log_level = 'DEBUG'  # if args.debug else 'INFO'
     logging.basicConfig(level=log_level, format='%(asctime)s %(levelname)s: %(message)s', stream=sys.stdout)
 
-    superset_insstances = [
+    superset_instances = [
         "superset-with-ldap-no-tls-node-default",
         "superset-with-ldap-insecure-tls-node-default",
         "superset-with-ldap-server-veri-tls-node-default",
-        "superset-with-ldap-mutual-veri-tls-node-default",
     ]
 
     for superset_instance in superset_instances:
-        http_code = requests.post(f"http://{superset_insstance}:8088/api/v1/security/login", json={
+        http_code = requests.post(f"http://{superset_instance}:8088/api/v1/security/login", json={
             "username": "integrationtest",
             "password": "integrationtest",
             "provider": "ldap",
