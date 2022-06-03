5cada81582165e17bc2c64b337fbbc24b7aa19b9
Malte Sander <malte.sander.it@gmail.com>
2022-06-03 15:52:40 +0200

    removed kafka tests

--- kafka-operator/.ci/integration-tests/hcloud-centos-8/test.sh
+++
@@ -1,8 +0,0 @@
-git clone -b $GIT_BRANCH https://github.com/stackabletech/integration-tests.git
-(cd integration-tests/kafka-operator && kubectl kuttl test)
-exit_code=$?
-./operator-logs.sh kafka > /target/kafka-operator.log
-./operator-logs.sh zookeeper > /target/zookeeper-operator.log
-./operator-logs.sh opa > /target/opa-operator.log
-./operator-logs.sh regorule > /target/regorule-operator.log
-exit $exit_code
