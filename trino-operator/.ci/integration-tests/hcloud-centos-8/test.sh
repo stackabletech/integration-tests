7c2588430c5b2a75f045c2a2243ae9b7d1f0e804
Malte Sander <malte.sander.it@gmail.com>
2022-06-06 10:39:23 +0200

    removed trino tests

--- trino-operator/.ci/integration-tests/hcloud-centos-8/test.sh
+++
@@ -1,6 +0,0 @@
-git clone -b $GIT_BRANCH https://github.com/stackabletech/integration-tests.git
-(cd integration-tests/trino-operator && kubectl kuttl test)
-exit_code=$?
-./operator-logs.sh trino > /target/trino-operator.log
-./operator-logs.sh hive > /target/hive-operator.log
-exit $exit_code
