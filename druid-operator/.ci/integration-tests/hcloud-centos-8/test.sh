92ae34d6bc9e45236798133e7a341998a7e4f695
Nikolaus Winter <mail@nikolaus-winter.de>
2022-06-29 11:51:22 +0200

    cleaned up integration tests

--- druid-operator/.ci/integration-tests/hcloud-centos-8/test.sh
+++
@@ -1,6 +0,0 @@
-git clone -b $GIT_BRANCH https://github.com/stackabletech/integration-tests.git
-(cd integration-tests/druid-operator && kubectl kuttl test)
-exit_code=$?
-./operator-logs.sh druid > /target/druid-operator.log
-./operator-logs.sh zookeeper > /target/zookeeper-operator.log
-exit $exit_code
--- nifi-operator/.ci/integration-tests/hcloud-centos-8/test.sh
+++
@@ -1,6 +0,0 @@
-git clone -b $GIT_BRANCH https://github.com/stackabletech/integration-tests.git
-(cd integration-tests/nifi-operator && kubectl kuttl test)
-exit_code=$?
-./operator-logs.sh nifi > /target/nifi-operator.log
-./operator-logs.sh zookeeper > /target/zookeeper-operator.log
-exit $exit_code
